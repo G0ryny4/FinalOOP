e2e35bd338f465ba22bb6779c674d25660c0c696
G0ryny4 <gorynych90@gmail.com>
2023-01-06 19:57:59 +0300

    added task for 2nd  seminar

--- seminar1/FamilyTree.java
+++ seminar1/FamilyTree.java
@@ -1,5 +1,51 @@
-//package FamilyTree;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FamilyTree {
 
+public class FamilyTree implements Serializable, IAddable {
+    private List<Human> humans;
+
+    public FamilyTree() {
+        this.humans = new ArrayList();
+    }
+    public List<Human> getHumans() {
+        return this.humans;
+    }
+
+    @Override
+    public void addHuman(Human human, Human father, Human mother) {
+        if (father != null) {
+            human.setFather(father);
+            father.getChildren().add(human);
+        }
+        if (mother != null) {
+            human.setMother(mother);
+            mother.getChildren().add(human);
+        }
+        humans.add(human);
+    }
+    public Human findByName(String name) {
+        for (Human human : humans) {
+            if (human.getName() == name)
+                return human;
+        }
+        return null;
+    }
+    public List<Human> findAllByName(String name) {
+        List<Human> humansList = new ArrayList();
+        for (Human human : humans) {
+            if (human.getName() == name)
+                humansList.add(human);
+        }
+        if (humansList != null)
+            return humansList;
+        else
+            return null;
+    }
+    public void showAllInConsole() {
+        for (Human human : humans) {
+            System.out.println(human.toString());
+        }
+    }
 }
