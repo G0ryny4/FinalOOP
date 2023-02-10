ce418fb6a77922d6859d78601be7ebb6506b6057
G0ryny4 <92444026+G0ryny4@users.noreply.github.com>
2023-02-10 19:25:49 +0300

    Delete seminar1 directory
    final task

--- seminar1/IFamilyTree.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface IFamilyTree<T extends LiveBeing<T>> {
-    List<T> getLiveBeings();
-
-    public void setLiveBeings(List<T> liveBeings);
-
-    void addLiveBeing(T liveBeing, T father, T mother);
-}
--- seminar1/TComparatorByDate.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.Comparator;
-
-public class TComparatorByDate<T extends LiveBeing<T>> implements Comparator<T> {
-    @Override
-    public int compare(T o1, T o2) {
-        return Integer.compare(o1.getDate(), o2.getDate());
-    }
-}
